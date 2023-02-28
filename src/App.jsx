207e5dcce7bb2ab1605e4af1b091d9048611bb22
Huydt13 <113592709+Huydt13@users.noreply.github.com>
2023-02-28 15:09:42 +0700

    feat : add task

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,44 @@ import reactLogo from "./assets/react.svg";
 import { ToDoList } from "./components/todoList";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      name: "Cong việc a",
+      isComplete: false,
+    },
+    {
+      id: 2,
+      name: "Cong việc b",
+      isComplete: false,
+    },
+    {
+      id: 3,
+      name: "Cong việc c",
+      isComplete: true,
+    },
+    {
+      id: 4,
+      name: "Cong việc d",
+      isComplete: true,
+    },
+  ]);
 
+  const onAdd = (name) => {
+    const task = {
+      id: Date.now(), // lấy thời gian thực
+      name,
+      isComplete: false,
+    };
+    setTodo([...todo, task]);
+  };
+  const onRemove = (i) => {
+    const task = todo.splice(i,1)
+    setTodo([...todo, task]);;
+  };
   return (
     <>
-      <ToDoList />
+      <ToDoList todoList={todo} onAdd={onAdd} onRemove={onRemove} />
     </>
   );
 }
--- src/components/todoList/index.jsx
+++ src/components/todoList/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const TodoListStyle = styled.div`
@@ -37,25 +37,63 @@ const TodoListStyle = styled.div`
     }
   }
 `;
-export const ToDoList = () => {
+export const ToDoList = ({ todoList, onAdd, onRemove }) => {
+  console.log("🚀 ~ file: index.jsx:41 ~ ToDoList ~ todoList:", todoList);
+  // const listDoing = todoList?.filter((e) => !e.isComplete);
+  // const listDone = todoList?.filter((e) => e.isComplete);
+  const [value, setValue] = useState("");
+  const [listDoing, setListDoing] = useState([]);
+  const [listDone, setListDone] = useState([]);
+
+  useEffect(() => {
+    if (todoList) {
+      console.log(todoList);
+      setListDoing(todoList?.filter((e) => !e.isComplete));
+      setListDone(todoList?.filter((e) => e.isComplete));
+    }
+  }, [todoList]);
+
+  const _onAdd = () => {
+    console.log(value);
+    onAdd(value);
+    setValue("");
+  }; //các component nào có tiền tố là _ là sử dụng nội bộ
+  // function này dùng để set lại giá trị không có khoảng trắng dư thừa ở đầu và cuối
   return (
     <TodoListStyle>
+      {console.log(todoList)}
       <div className="input-group">
-        <input type="text" placeholder="Task" />
-        <button onClick={() => console.log("dsada")}>Add</button>
+        <input
+          value={value}
+          onChange={(ev) => setValue(ev.target.value)}
+          type="text"
+          placeholder="Task"
+        />
+        {/* disable = !value.trim() làm cho button hiển thị khi có giá trị trong input  */}
+        {/* hàm trim() ngăn button hiển thị khi input được nhập khoảng trắng */}
+        <button onClick={_onAdd} disabled={!value.trim()}>
+          Add
+        </button>
       </div>
       <div className="list-board">
         <div className="board">
           <div className="title">Công việc đang làm</div>
           <div className="items">
-            <TodoItems />
+            {listDoing.map((e) => (
+              <>
+                <TodoItems key={e.id} {...e} />
+                <button onClick={onRemove}>Remove</button>
+              </>
+            ))}
           </div>
         </div>
         <div className="board"></div>
         <div className="board">
           <div className="title">Công việc đã hoàn thành</div>
           <div className="items">
-            <TodoItems />
+            {listDone.map((e) => (
+              <TodoItems key={e.id} {...e} />
+            ))}
           </div>
         </div>
         <div className="board"></div>
@@ -86,10 +124,10 @@ const ToDoItemsStyle = styled.div`
   }
 `;
 
-const TodoItems = () => {
+const TodoItems = ({ id, name, isComplete }) => {
   return (
-    <ToDoItemsStyle className="isComplete">
-      <div className="name">Công việc A</div>
+    <ToDoItemsStyle className={isComplete ? "isComplete" : ""}>
+      <div className="name">{name}</div>
       <button>✓</button>
     </ToDoItemsStyle>
   );
